d3cb193cfa07cbcdedb7c5b25641af5b1c008e6d
anuragbakshi <anuragbakshi123@gmail.com>
2015-07-31 21:11:30 -0400

    Added random utils

---
+++ random.js
@@ -0,0 +1,9 @@
+window.random = {};
+
+random.rand = function(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+random.randInt = function(min, max) {
+	return Math.floor(random(min, max + 1));
+}
